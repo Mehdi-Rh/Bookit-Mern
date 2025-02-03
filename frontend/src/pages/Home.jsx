3ab6dcd3869b6d4b7beaa8397333f6c229a4e420
Mehdi Rahal <mehdi@lablabee.com>
2025-02-03 22:33:26 +0100

    Fetch and display available rooms from API in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import rooms from "../data/rooms";
 import RoomCard from "../components/RoomCard";
 import Heading from "../components/Heading";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   // const rooms = await getAllRooms();
 
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const getAllRooms = async () => {
+      const response = await fetch("http://localhost:5000/api/rooms/");
+      const data = await response.json();
+      setRooms(data.rooms);
+    };
+
+    getAllRooms();
+  }, []);
+
   return (
     <>
-      {" "}
       <Heading title="Available Rooms" />
       {rooms.length > 0 ? (
-        rooms.map((room) => <RoomCard room={room} key={room.$id} />)
+        rooms.map((room) => <RoomCard room={room} key={room._id} />)
       ) : (
         <p>No rooms available at the moment</p>
       )}
