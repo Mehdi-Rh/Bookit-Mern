3b9e52b6faf331c3af42117de8660dabfea5740d
Mehdi Rahal <mehdi@lablabee.com>
2025-05-24 19:25:35 +0100

    Add mobile navbar button conditionally based on user authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,31 +19,33 @@ const Header = () => {
     <header className="bg-gray-100 w-full">
       <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          <button
-            data-collapse-toggle="navbar-hamburger"
-            type="button"
-            className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-hamburger"
-            aria-expanded="false"
-            onClick={() => setOpenMobileNavbar(!openMobileNavbar)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+          {user && (
+            <button
+              data-collapse-toggle="navbar-hamburger"
+              type="button"
+              className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-hamburger"
+              aria-expanded="false"
+              onClick={() => setOpenMobileNavbar(!openMobileNavbar)}
             >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          )}
           <div className="flex items-center">
             <Link onClick={() => setOpenMobileNavbar(false)} to="/">
               <img className="h-12 w-12" src={logo} alt="Bookit" />
